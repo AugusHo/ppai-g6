1f90195cabad34f963de6ee1c618f21b1dbf2c9f
hohlke90263 <90263@sistemas.frc.utn.edu.ar>
2024-05-22 15:41:45 -0300

    primero

---
+++ PantallaImportarActualizacion/Entidades/Bodega.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantallaImportarActualizacion.Entidades
+{
+    public class Bodega
+    {
+        private string descripcion;
+        private string historia;
+        private string nombre;
+        private bool periodoActualizacion;
+        private int coordenadasUbicacion;
+
+        public Bodega(string descripcionBodega, string historiaBodega, string nombreBodega, bool periodoActualizacionBodega, int coordenadasUbicacionBodega)
+        {
+            descripcion = descripcionBodega;
+            historia = historiaBodega;
+            nombre = nombreBodega;
+            periodoActualizacion = periodoActualizacionBodega;
+            coordenadasUbicacion = coordenadasUbicacionBodega;
+
+        }
+
+        public string descripcionBodega
+        {
+            get => descripcion;
+            set => descripcion = value;
+        }
+
+        public string historiaBodega
+        {
+            get => historia;
+            set => historia = value;
+        }
+
+        public string nombreBodega
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public bool periodoActualizacionBodega
+        {
+            get => periodoActualizacion;
+            set => periodoActualizacion = value;
+        }
+
+        public int coordenadasUbicacionBodega
+        {
+            get => coordenadasUbicacion;
+            set => coordenadasUbicacion = value;
+        }
+
+        public List<Bodega> bodegas { get; set; }
+
+        //Metodos
+
+        public bool estaParaActualizarNovedadesVino()
+        {
+            return periodoActualizacion;
+        }
+    }
+}
---
+++ PantallaImportarActualizacion/ImportarActualizacionVino.cs
@@ -0,0 +1,27 @@
+﻿using PantallaImportarActualizacion.Entidades;
+using System.Windows.Forms;
+
+namespace PantallaImportarActualizacion
+{
+    public partial class ImportarActualizacionVino : Form
+    {
+        private GestorImportadorBodega gestor = new GestorImportadorBodega();
+
+        public ImportarActualizacionVino()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void habilitarPantalla()
+        {
+            this.Show();
+            cmbBodegas.DataSource = gestor.buscarBodegasConActualizacionesPendientes();
+        }
+
+        private void cmbBodegas_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            gestor.tomarSeleccionBodega(cmbBodegas.SelectedItem.ToString());
+        }
+    }
+}
